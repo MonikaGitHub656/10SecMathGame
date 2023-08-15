284b3107aeea8175b88874f2d8bc17fadaab4dff
Monika <monikamessages@gmail.com>
2023-08-15 13:02:42 -0700

    completed all requirements of project

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var currentQuestion;
 var timeLeft = 10;
+var interval;
+var score = 0;
+var maxNum;
+
 
 var randomNumberGenerator = function (size) {
   return Math.ceil(Math.random() * size);
@@ -20,6 +24,8 @@ var checkAnswer = function (userInput, answer) {
   if (userInput === answer) {
     renderNewQuestion();
     $('#user-input').val('');
+    updateTimeLeft(+1);
+    updateScore(+1);
   };
 }
 
@@ -28,18 +34,49 @@ var renderNewQuestion = function () {
   $('#equation').text(currentQuestion.equation);
 }
 
+var updateTimeLeft = function (amount) {
+  timeLeft += amount;
+  $('#time-left').text(timeLeft);
+}
+
+var startGame = function () {
+  if (!interval) {
+    if (timeLeft === 0) {
+      updateTimeLeft(10);
+      updateScore(-score);
+    }
+    interval =  setInterval(function () {
+      updateTimeLeft(-1);
+      if (timeLeft === 0) {
+        clearInterval(interval);
+        interval = undefined;
+      }
+    }, 1000);
+  }
+}
+
+var updateScore = function (amount) {
+  score += amount;
+  $('#score').text(score);
+}
+
+var updateRange = function () {
+
+}
+
 $(document).ready(function () {
   renderNewQuestion();
 
-  var interval =  setInterval(function () {
-    timeLeft--;
-    $('#time-left').text(timeLeft);
-    if (timeLeft === 0) {
-      clearInterval(interval);
-    }
-  }, 1000);
+  // listening for change in user input for range to use in number generator
+  var value = document.querySelector("#value");
+  var range = document.querySelector("#range");
+  value.textContent = range.value;
+  $('#range').on("change", function (e) {
+    maxNum = e.target.value;
+  });
 
   $('#user-input').on('keyup', function () {
+    startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
 
   });
